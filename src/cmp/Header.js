23c146bd6305c7a4ff12d006df1e16e0755a3063
Bohdan <bohdan.romashchenko@gmail.com>
2020-09-26 16:19:09 +0300

    header

--- src/cmp/Header.js
+++ src/cmp/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import img from './65b.jpg'
+import Form from'react-bootstrap/Form'
+import Button from "react-bootstrap/Button";
+import {useHistory} from 'react-router-dom'
+
 export default function Header() {
+    const history = useHistory();
 
     return(
         <>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand>
+                <Navbar.Brand onClick={()=>history.push('/')}>
                     <img
                         alt=""
                         src={img}
@@ -16,6 +21,12 @@ export default function Header() {
                     />{' '}
                     Virtual Bricks
                 </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Form inline >
+                        <Button variant={'secondary'} onClick={()=>history.push('/create')}>Create project</Button>
+                    </Form>
+                </Navbar.Collapse>
             </Navbar>
         </>
     )

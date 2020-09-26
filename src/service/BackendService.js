32d88c96eea6efa154dec2bb7f47d22589a1ca17
Bohdan <bohdan.romashchenko@gmail.com>
2020-09-26 15:54:09 +0300

    start adding service

---
+++ src/service/BackendService.js
@@ -0,0 +1,26 @@
+class BackendService{
+
+    async createProject(projectDTO){
+
+        try {
+            await fetch('https://localhost:5001/api/project/create', {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                body: JSON.stringify(projectDTO) // body data type must match "Content-Type" header
+            });
+            return true;
+        }
+        catch (e) {
+            return false;
+        }
+    }
+
+    async getProject(pin){
+        try {
+            const res = await fetch('https://localhost:5001/api/project/' + pin);
+            return res.json();
+        }
+        catch (e) {
+            return '';
+        }
+    }
+}
